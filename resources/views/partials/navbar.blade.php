68109a41dac5f71f1b820fa2d1c9a5f408fd4352
Timothy Kristanto <66807730+TimothyKristanto@users.noreply.github.com>
2022-03-14 18:42:26 +0700

    Fixed navbar + finished tryout index page

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,13 @@
-<nav class="nav navbar-expand-sm">
-    <div class="d-flex bg-navbar w-100 pt-3 px-2 pb-2 align-items-center">
-      <a href="/" class="navbar-brand d-flex justify-align-start mx-2 w-25 fw-bold">
-        <img src="/image/logo.png" width="30" height="30" class="mr-3 ms-1" >
+{{-- <nav class="nav navbar-expand-sm bg-navbar">
+    <div class="pt-2 px-2 pb-2 d-flex">
+      <a href="/" class="navbar-brand d-flex justify-align-start mx-2 fw-bold">
+        <img src="/image/logo.png" width="30" height="30" class="me-3 ms-1" >
         Online Class
       </a>
-
-      <div class="collapse navbar-collapse d-flex justify-content-end w-80" id="collapsibleNavbar">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-end float-end" id="collapsibleNavbar">
         <ul class="navbar-nav mx-4">
           <li class="nav-item mx-1 ">
             <a class="nav-link {{ $page == "beranda" ? "active" : "" }}" href="/">Beranda</a>
@@ -22,10 +24,52 @@
           <li class="nav-item mx-1 ">
             <a class="nav-link {{ $page == "bank_soal" ? "active" : "" }}" href="#">Bank Soal</a>
           </li>
+          <li class="nav-item mx-2">
+            <a href="#" class="nav-link">
+              <img src="/image/profpic.png" width="35" height="35" class="prof-img">
+            </a>
+          </li>
         </ul>
-
-        <a href="#" class="mx-0"><img src="#" class="/image/profpic.png"></a>
-
       </div>
     </div>
+</nav> --}}
+
+<nav class="navbar navbar-expand-sm bg-navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex mx-2" href="/">
+      <img src="/image/logo.png" width="30" height="30">
+      Online Class
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+      <ul class="navbar-nav align-items-center">
+        <li class="nav-item mx-1 ">
+          <a class="nav-link {{ $page == "beranda" ? "active" : "" }}" href="/">Beranda</a>
+        </li>
+        <li class="nav-item mx-1 ">
+          <a class="nav-link {{ $page == "tryout" ? "active" : "" }}" href="/dashboard/tryout">Tryout</a>
+        </li>
+        <li class="nav-item mx-1 ">
+          <a class="nav-link {{ $page == "materi" ? "active" : "" }}" href="#">Materi</a>
+        </li>
+        <li class="nav-item mx-1 ">
+          <a class="nav-link {{ $page == "kalender" ? "active" : "" }}" href="#">Kalender</a>
+        </li>
+        <li class="nav-item mx-1 ">
+          <a class="nav-link {{ $page == "bank_soal" ? "active" : "" }}" href="#">Bank Soal</a>
+        </li>
+        <li class="nav-item dropdown justify-content-end">
+          <a class="nav-link dropdown-toggle d-flex align-items-center" id="dropdownMenuLink" href="#" role="button" data-bs-toggle="dropdown">
+            <img src="/image/profpic.png" width="35" height="35" class="prof-img">
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Setting</a></li>
+            <li><a class="dropdown-item" href="#">Log out</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
