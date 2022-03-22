a251649e85393d8efc7489851da54443aa0b0039
AverillSaladin03 <76471341+AverillSaladin03@users.noreply.github.com>
2022-03-22 20:23:21 +0700

    update visual tryout (2 halaman)

--- resources/views/dashboard/tryout/index.blade.php
+++ resources/views/dashboard/tryout/index.blade.php
@@ -4,22 +4,22 @@
     <div class="row justify-content-center text-center align-items-center">
         <h1 class="h1 mt-4">Latih kemampuanmu yuk!</h1>
         <div class="col-4 mt-4">
-            <a href="/dashboard/result" class="btn btn-warning w-75 rounded-corners">
+            <a href="/dashboard/result" class="btn btn-auth w-75 rounded-corners">
                 <div class="d-flex justify-content-center align-items-center">
-                    Cek hasil tryout mu disini 
-                    <i class="bi bi-book icon ms-2"></i>
-                </div> 
+                    Cek hasil tryout mu disini
+                    <i class="bi bi-book icon ms-3"></i>
+                </div>
             </a>
         </div>
         <div class="col-4 mt-4">
-            <a class="btn btn-warning w-75 rounded-corners">
+            <a class="btn btn-auth w-75 rounded-corners">
                 <div class="d-flex justify-content-center align-items-center">
-                    Pelajari materinya disini 
-                    <i class="bi bi-book icon ms-2"></i>
-                </div> 
+                    Pelajari materinya disini
+                    <i class="bi bi-book icon ms-3"></i>
+                </div>
             </a>
         </div>
-        <div class="border border-3 border-warning my-4 w-75 px-5 py-4 rounded-corners">
+        <div class="border border-3 glass-area my-4 w-75 px-5 py-4 rounded-corners">
             @for ($i = 0; $i < 10; $i++)
                 <div class="my-4 row align-items-center">
                     <div class="col d-flex">
@@ -27,14 +27,14 @@
                         <p class="ms-3">120 menit</p>
                     </div>
                     <div class="col d-flex justify-content-end">
-                        <button class="btn btn-warning">
+                        <button class="btn btn-auth">
                             Kerjakan
                         </button>
                     </div>
                 </div>
-                
+
             @endfor
         </div>
     </div>
-    
+
 @endsection
